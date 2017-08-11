04fd2113fd48bed5ac91d788ff78e7c9cb52f4f3
Will Carpenter <willcarpenteriii@gmail.com>
2017-08-11 15:36:45 -0600

    added mongoose model for todos

--- playground/connect.js
+++ playground/connect.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectID } = require('mongodb')
+const { MongoClient } = require('mongodb')
 const url = 'mongodb://localhost:27017/TodoApp'
 
 MongoClient.connect(url, (err, db) => {
---
+++ playground/delete.js
@@ -0,0 +1,27 @@
+const { MongoClient, ObjectID } = require('mongodb')
+const url = 'mongodb://localhost:27017/TodoApp'
+
+MongoClient.connect(url, (err, db) => {
+	if(err) {
+		return console.log('unable to connect to mongo server')
+	}
+	console.log('Connected to Mongo server')
+
+	//  delete Many
+	// db.collection('Todos').deleteMany({text: 'Walk kela'})
+	// 	.then(outcome => {
+	// 		console.log(outcome.result['ok'])
+	// 	})
+
+
+	//  delete One
+	db.collection('Todos').deleteOne({text: 'Walk kela'})
+		.then(results => {
+			console.log(results.result)
+		})
+	//  find One and Delete
+
+
+
+	//db.close()
+})
